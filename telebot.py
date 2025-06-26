83bd77ca240c6b30b301497481a6de3b9909b350
Ican <icannskk9@gmail.com>
2025-06-26 15:15:02 +0700

    PUSH

--- telebot.py
+++ telebot.py
@@ -39,9 +39,11 @@ cookies = {
 }
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'Referer': 'https://loyalty.aldmic.com/reward'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9",
+    "Referer": "https://loyalty.aldmic.com/",
+    "Origin": "https://loyalty.aldmic.com",
+    "Connection": "keep-alive",
 }
 
 url = 'https://loyalty.aldmic.com/reward?cat=11'
